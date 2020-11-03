077b5552d56c0c0a9ea642d3fa2aa010e6168c6c
rajabovjavid <jvdrjbv@gmail.com>
2020-11-03 15:42:09 +0400

    ShowScreen (details of blog post) is added

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
+import { Context } from "../context/BlogContext";
+
+const ShowScreen = ({ navigation }) => {
+
+  const {state} = useContext(Context)
+
+  const blogPost = state.find((blogPost)=>blogPost.id===navigation.getParam("id"))
+
+  return (
+    <View>
+      <Text>{blogPost.title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ShowScreen;
